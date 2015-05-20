0710f8c93bd8018e8534f2eb9d194badaa94f2a7
Yong Bakos <ybakos@humanoriented.com>
2015-05-20 16:28:30 +0200

    Lesson 10. Persist last selected row in NSUserDefaults. Refactor picker view row selection into multiple reusable methods. Extract plist key into constant.

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate {
 
+    let userDefaultsLastRowKey = "defaultCelsiusPickerRow"
+    
     @IBOutlet var temperatureRange: TemperatureRange!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var celsiusPicker: UIPickerView!
+    
     private let converter = UnitConverter()
     
     override func viewDidLoad() {
@@ -33,9 +36,20 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int,
         inComponent component: Int) {
+        displayConvertedTemperatureForRow(row)
+        saveSelectedRow(row)
+    }
+    
+    func displayConvertedTemperatureForRow(row: Int) {
         let degreesCelsius = temperatureRange.values[row]
         temperatureLabel.text = "\(converter.degreesFahrenheit(degreesCelsius))°F"
     }
 
+    func saveSelectedRow(row: Int) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(row, forKey: userDefaultsLastRowKey)
+        defaults.synchronize()
+    }
+    
 }
 
