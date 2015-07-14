fd8de9f187fe21ac216597c428592ccc99aa3f0d
Bishal Ghimire <bishal.ghimire@gmail.com>
2015-07-14 17:42:11 +0545

    MARK, Comment added Started using MARK to ceperate the code levels. Comment added for explaning the initial Picker Row count method. Added an extra line of code for Gahrenheit Value so it easier to read the final convertion text code

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -17,10 +17,12 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     
     private let converter = UnitConverter()
     
+    // MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let row = initialPickerRow()
-        celsiusPicker.selectRow(row, inComponent: 0, animated: false)
+        celsiusPicker.selectRow(row, inComponent: 0, animated: true)
         pickerView(celsiusPicker, didSelectRow: row, inComponent: 0)
     }
 
@@ -29,6 +31,8 @@ class ViewController: UIViewController, UIPickerViewDelegate {
         if let row = savedRow  {
             return row
         } else {
+            // we have 200 total rows from -100 to +100 so to make middle value 0 selection
+            // we have to get half of total row counts i.e. 200 / 2 = 100
             return celsiusPicker.numberOfRowsInComponent(0) / 2
         }
     }
@@ -37,6 +41,8 @@ class ViewController: UIViewController, UIPickerViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UIPickerViewDelegate
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         let celsiusValue = temperatureRange.values[row]
@@ -51,7 +57,8 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     
     func displayConvertedTemperatureForRow(row: Int) {
         let degreesCelsius = temperatureRange.values[row]
-        temperatureLabel.text = "\(converter.degreesFahrenheit(degreesCelsius))°F"
+        let degreesFahrenheit = converter.degreesFahrenheit(degreesCelsius)
+        temperatureLabel.text = "\(degreesFahrenheit)°F"
     }
 
     func saveSelectedRow(row: Int) {
